a968cfc134705d4fef34ce6f19554157123aacdd
konyadonat <konya.donat@gmail.com>
2022-07-11 10:45:13 +0200

    User class created for validation

--- thesis/app/src/main/java/com/konyadonat/thesis/user/User.java
+++ thesis/app/src/main/java/com/konyadonat/thesis/user/User.java
@@ -1,2 +1,41 @@
-package com.konyadonat.thesis.user;public class User {
+package com.konyadonat.thesis.user;
+
+public class User {
+    private String email;
+    private String password;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    //TODO it works but should be replaced
+    public String getPassword() {
+        return password;
+    }
+
+    public static class UserBuilder{
+        User user = new User();
+
+        public UserBuilder setEmail(String email){
+            user.email = email;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password){
+            user.password = password;
+            return this;
+        }
+
+        public UserBuilder setAge(int age){
+            user.age = age;
+            return this;
+        }
+        public User getUser() {
+            return user;
+        }
+    }
 }
