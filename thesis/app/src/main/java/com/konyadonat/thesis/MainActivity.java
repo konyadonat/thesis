e6aa607d45448e9a5d6528c1a50285e4a7ee9517
konyadonat <konya.donat@gmail.com>
2022-07-11 10:45:58 +0200

    MainActivity.java Views declared

--- thesis/app/src/main/java/com/konyadonat/thesis/MainActivity.java
+++ thesis/app/src/main/java/com/konyadonat/thesis/MainActivity.java
@@ -3,6 +3,9 @@ package com.konyadonat.thesis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText email = findViewById(R.id.editTextTextEmailAddress);
+        EditText password = findViewById(R.id.editTextTextPassword);
+        Button login = findViewById(R.id.btn_Login);
+        Button register = findViewById(R.id.btn_Register);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Its should check if the data is in the database
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Route to the register screen, then validate input data
+            }
+        });
     }
 }
