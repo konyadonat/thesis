42053a599b059cb941d6ebb7a4fb51cabc568c68
konyadonat <konya.donat@gmail.com>
2022-07-11 15:23:55 +0200

    RegisterActivity validation started

--- thesis/app/src/main/java/com/konyadonat/thesis/RegisterActivity.java
+++ thesis/app/src/main/java/com/konyadonat/thesis/RegisterActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.konyadonat.thesis.user.Exception.NameTooShortException;
+import com.konyadonat.thesis.user.Exception.PasswordAgainNotTheSameException;
+import com.konyadonat.thesis.user.User;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -20,6 +25,31 @@ public class RegisterActivity extends AppCompatActivity {
         EditText name = findViewById(R.id.editTextTextPersonName);
         Button register = findViewById(R.id.btn_registerInReigsterActivity);
         Button back = findViewById(R.id.btn_backToLoginInRegister);
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    if (password != passwordAgain) {
+                        throw new PasswordAgainNotTheSameException("A jelszó és a jelszó újra nem egyezik!");
+                    }
+                    User temp = new User.UserBuilder()
+                            .setEmail(email.getText().toString())
+                            .setPassword(password.getText().toString())
+                            .setName(name.getText().toString())
+                            .getUser();
+                } catch (NameTooShortException e) {
+                    Toast.makeText(RegisterActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+
+                }
+                catch (Exception e){
+                    Toast.makeText(RegisterActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
 
 
 
